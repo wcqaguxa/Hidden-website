eaa1301c02874d9555612c2fb21c2019e4512cd7
wcqaguxa <a.k.wolska@student.tudelft.nl>
2020-04-28 21:47:13 +0200

    edited the template to not link unexisting pages. working on links.

--- main/server.js
+++ main/server.js
@@ -24,20 +24,30 @@ app.get('/\\/page\\/(\\d)+(\\/*)*/', function(req, res) {
     const path = url.parse(req, true).pathname.split("/");
     console.log("pathname is: "+path);
 
-    const pageNum = parseInt(path[1]);
+    let pageNum = parseInt(path[1]);
     if (pageNum >= pagesTable.length) {
         //error
-        console.log("Error. Page number too high: "+pageNum);
-    } else {
-        //serve page
-        res.render('index', {picture_url: '../../'+pagesTable[pageNum]} );
-        console.log("Page url sent.");
+        console.log("Error. Page number too high: " + pageNum);
+        pageNum = pagesTable.length -1
     }
+    //serve page
+    res.render('index', {picture_url: '../../'+pagesTable[pageNum],
+             first: pageNum === 0, last: pageNum === pagesTable.length-1,
+        prevP: pageNum-1, nextP: pageNum+1, lastP:pagesTable.length-1} );
+    console.log("Page url sent.");
+
     res.send();
 });
 
-//anything, including the web page
+//serves the first page. when start posting, change to last page
 app.get('/', function(req, res) {
    console.log("Serving the first page");
-    res.render('index', {picture_url: pagesTable[0]});
+    res.render('index', {picture_url: pagesTable[0], first:true, last:false,
+                        prevP: 0, nextP: 1, lastP:pagesTable.length-1});
+});
+
+//serves the first page. when start posting, change to last page
+app.get('/*', function(req, res) {
+    const toPrint = url.parse(req, true).pathname
+    console.log(toPrint);
 });
